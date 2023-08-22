87862a366b13037ae38ffa6cb8c8cd041cfebaa8
PRAKHAR BAJPAI <138972184+prakhar-iitbhu@users.noreply.github.com>
2023-08-22 17:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def index():
             logging.info("log my final result {}".format(reviews))
 
             
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://prakhar-iitbhu:Prakh_54627@cluster0.yd3popi.mongodb.net/?retryWrites=true&w=majority")
             db =client['scrapper_eng_pwskills']
             coll_pw_eng = db['scraper_pwskills_eng']
             coll_pw_eng.insert_many(reviews)
